56c554efd48a62a59c3e0835dbe5edc1c749f121
Shane Kilkelly <shane@kilkelly.me>
2017-08-12 20:17:10 +0100

    Update mix

--- mix.exs
+++ mix.exs
@@ -4,6 +4,13 @@ defmodule HammerBackendEcto.Mixfile do
   def project do
     [
       app: :hammer_backend_ecto,
+      description: "Ecto backend for Hammer rate-limiter",
+      package: [name: :hammer_backend_ecto,
+                maintainers: ["Shane Kilkelly (shane@kilkelly.me)"],
+                licenses: ["MIT"],
+                links: %{"GitHub" => "https://github.com/ExHammer/hammer-backend-ecto"}],
+      source_url: "https://github.com/ExHammer/hammer-backend-ecto",
+      homepage_url: "https://github.com/ExHammer/hammer-backend-ecto",
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
@@ -20,9 +27,8 @@ defmodule HammerBackendEcto.Mixfile do
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-    ]
+    [{:redix, "~> 0.6"},
+     {:mock, "~> 0.2.0", only: :test},
+     {:ex_doc, "~> 0.16", only: :dev}]
   end
 end
